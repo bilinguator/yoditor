0c4b8e6b1ff6c5a22a086adf8e163d46b5d8a152
Dmitrii Snitkin <snitkin.d@list.ru>
2023-09-16 13:21:29 +0300

    Implement function replace_by_regex

--- yoditor.py
+++ yoditor.py
@@ -1,8 +1,9 @@
 import os
+import re
 
 """Uploading two lists of Russian words:
-`yo_sure` - words where <Ё> letter is 100% certain;
-`yo_unsure` - words with uncertianty about <Ё> letters.
+list `yo_sure` - words where <Ё> letter is 100% certain;
+list `yo_unsure` - words with uncertianty about <Ё> letters.
 """
 
 yo_sure_path = 'yobase/yo_sure.txt'
@@ -17,3 +18,19 @@ with open(yo_sure_path, 'r', encoding='UTF-8') as file:
     yo_sure = file.read().split()
 with open(yo_unsure_path, 'r', encoding='UTF-8') as file:
     yo_unsure = file.read().split()
+
+def replace_by_regex (text, regex, old, new):
+    """Replace old substring to new one inside the hits found by regular expression.
+    
+    str `regex` - string with regular expression for searching hits by re.finditer;
+    str `old` - string to be replace inside the hits found by regex;
+    str `new` - target replacement;
+    return: str - text with replacements in the hits.
+    """
+    hits = re.finditer(regex, text)
+    for hit in hits:
+        start = hit.start()
+        end = hit.end()
+        text = text[:start] + text[start:end].replace(old, new) \
+            + text[end:]
+    return text
